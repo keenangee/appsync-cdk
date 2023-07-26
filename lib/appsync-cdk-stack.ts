a113382d7001a66c83f3a2bfb78982ef15b05a10
Keenan Gee <k33nang33@hotmail.co.uk>
2023-07-26 16:02:13 +0100

    schema added

--- lib/appsync-cdk-stack.ts
+++ lib/appsync-cdk-stack.ts
@@ -1,16 +1,45 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import * as appsync from "aws-cdk-lib/aws-appsync";
+import * as cognito from "aws-cdk-lib/aws-cognito";
 
 export class AppsyncCdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create the Cognito User Pool
+    const userPool = new cognito.UserPool(this, "CognitoUserPool", {
+      selfSignUpEnabled: false,
+      autoVerify: { email: true },
+      passwordPolicy: {
+        minLength: 8,
+        requireLowercase: false,
+        requireUppercase: false,
+        requireDigits: false,
+        requireSymbols: false,
+      },
+      signInAliases: { email: true },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AppsyncCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Create the AppSync API
+    const api = new appsync.GraphqlApi(this, "AppSyncApi", {
+      name: "appsync-backend",
+      logConfig: {
+        fieldLogLevel: appsync.FieldLogLevel.ALL,
+      },
+      schema: appsync.SchemaFile.fromAsset("schema.api.graphql"),
+      authorizationConfig: {
+        defaultAuthorization: {
+          authorizationType: appsync.AuthorizationType.USER_POOL,
+          userPoolConfig: {
+            userPool,
+          },
+        },
+      },
+    });
+
+    // Output the Cognito User Pool ID
+    new cdk.CfnOutput(this, "CognitoUserPoolId", {
+      value: userPool.userPoolId,
+    });
   }
 }
